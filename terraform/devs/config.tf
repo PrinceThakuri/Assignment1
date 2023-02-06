70dd88e21d1100b5f82c0ba362bbd2eb5687a111
Prinnce Thakuri <mrdeft1997@gmail.com>
2023-02-06 19:16:28 +0000

    deleted config.tf

--- terraform/devs/config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "clo835-prince"                // Bucket where to SAVE Terraform State
-    key    = "dev/bucket/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
-    region = "us-east-1"                           // Region where bucket is created
-  }
-}
